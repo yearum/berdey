8ec0f0c9144000df75d974d9e14d6a8fea0f7d15
yeruum <137423874+yearum@users.noreply.github.com>
2023-11-23 19:52:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttonLampu = document.getElementById('lampu');
     const buttonPesan = document.getElementById('pesan');
 
-    // Fungsi untuk menampilkan layar gelap
+    
     function showOverlay() {
         overlay.style.display = 'block';
     }
 
-    // Fungsi untuk menyalakan lampu
+    
     function toggleLampu() {
         lampuElement.classList.toggle('lampu-nyala');
     }
 
-    // Fungsi untuk menampilkan pesan
+    
     function showPesan() {
         pesanElement.style.display = 'block';
         videoPlayer.style.display = 'block';
         videoPlayer.play();
     }
 
-    // Tombol untuk menyalakan lampu
+    
     buttonLampu.addEventListener('click', toggleLampu);
 
-    // Tombol untuk menampilkan pesan
+    
     buttonPesan.addEventListener('click', showPesan);
 
-    // Menampilkan layar gelap saat halaman dimuat
+   
     showOverlay();
 });
